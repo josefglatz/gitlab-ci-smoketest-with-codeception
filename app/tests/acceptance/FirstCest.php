61319c7841b6d21752402ddcb11d6786ae87ce8d
Josef Glatz <josefglatz@gmail.com>
2019-09-19 16:48:33 +0200

    [TASK] Update labels

--- app/tests/acceptance/FirstCest.php
+++ app/tests/acceptance/FirstCest.php
@@ -2,11 +2,21 @@
 
 class FirstCest
 {
+    /**
+     * Check the homepage if the text "Flexible Content Management System" can be found
+     *
+     * @param AcceptanceTester $I
+     */
     public function frontpageWorks(AcceptanceTester $I)
     {
         $I->amOnPage('/');
         $I->see('Flexible Content Management System');
     }
+
+    /**
+     * Check the TYPO3 backend login test
+     * @param AcceptanceTester $I
+     */
     public function backendloginpageWorks(AcceptanceTester $I)
     {
         $I->amOnPage('/typo3/');
